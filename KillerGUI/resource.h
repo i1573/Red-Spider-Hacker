9dc8beaf3bc5f0d81878ac5290cf06dcf0360634
Casper <42424462+SkyNet-PKN@users.noreply.github.com>
2022-07-23 20:12:17 +0800

    Add files via upload

---
+++ KillerGUI/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 KillerGUI.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_KILLERGUI_DIALOG            102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_KILLERGUI                   107
+#define IDI_SMALL                       108
+#define IDC_KILLERGUI                   109
+#define IDC_NOTIFYICON                  110
+#define IDR_MAINFRAME                   128
+#define ID_TSKEn                        32771
+#define ID_TSKDis                       32772
+#define ID_32773                        32773
+#define ID_Window                       32774
+#define ID_SHOW                         32775
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           111
+#endif
+#endif
